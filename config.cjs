cdb91ca71c4a27eb1afe6b40323b180c0f9d1ac0
Chea7412 <samuelchea0@gmail.com>
2024-08-06 18:42:24 +0300

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -19,7 +19,7 @@ const config = {
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
   OWNER_NAME: process.env.OWNER_NAME || "✪𝘐'𝘮 𝘤𝘩𝘦𝘢༒",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "+254745294543",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "254745294543",
   GEMINI_KEY: process.env.GEMINI_KEY || "",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
